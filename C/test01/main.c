8a7614ec7e86cd2f5c0e80abcb5e7d922b576b7a
Seven-milk <786909151@qq.com>
2021-06-11 02:31:46 -0700

    test01 sin

--- C/test01/main.c
+++ C/test01/main.c
@@ -16,8 +16,13 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <math.h>
 
 void main()
 {
-	printf("hello world!\n");
+		float x,s;
+		printf("input number:\n");
+		scanf("%1f", &x);
+		s=sin(x);
+		printf("sin of %1f is %1f\n", x, s);
 };
