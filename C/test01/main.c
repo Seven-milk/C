3b90b7f86ae1058f9a18f5d0ed303542a77ba4a6
Seven-milk <786909151@qq.com>
2021-06-11 02:02:52 -0700

    helloword

---
+++ C/test01/main.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description: test01 
+ *
+ *        Version:  1.0
+ *        Created:  06/11/2021 01:50:41 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+void main()
+{
+	printf("hello world!\n");
+};
