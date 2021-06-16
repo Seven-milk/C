17d1368b3a4f22cb3efdd680a981a8b559943c4a
Seven-milk <786909151@qq.com>
2021-06-16 11:47:26 +0800

    test array

--- C/test01/main.c
+++ C/test01/main.c
@@ -20,12 +20,13 @@
 
 void main()
 {
-	for(int i=0; i<=5; i++)
+	int a[3];
+	for(int i=0; i<3; i++)
 	{
-		for(int j=0; j<i; j++)
-		{
-			printf("*");
-		}
-		printf("\n");
+		scanf("%d", &a[i]);
+	}
+	for(int i=0; i<3; i++)
+	{
+		printf("%d\n", a[i]);
 	}
 }
