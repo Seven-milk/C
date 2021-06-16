6bdb0731f446c0e9cc5f8ab716fccacc4a1cf7d0
Seven-milk <786909151@qq.com>
2021-06-16 12:21:41 +0800

    test 2D array

--- C/test01/main.c
+++ C/test01/main.c
@@ -20,13 +20,20 @@
 
 void main()
 {
-	int a[3];
+	int a[3][4]={1,2,3,4,5,6,7,8,9,10,11,12}, b[4][3];
 	for(int i=0; i<3; i++)
 	{
-		scanf("%d", &a[i]);
+		for(int j=0; j<4; j++)
+		{
+			b[j][i]=a[i][j];
+		}
 	}
-	for(int i=0; i<3; i++)
+	for (int i=0; i<3; i++)
 	{
-		printf("%d\n", a[i]);
+		printf("%d %d %d %d\n", a[i][0], a[i][1], a[i][2], a[i][3]);
+	}
+	for(int i=0; i<4; i++)
+	{
+		printf("%d %d %d\n", b[i][0], b[i][1], b[i][2]);
 	}
 }
