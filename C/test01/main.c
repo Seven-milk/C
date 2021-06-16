c35c31a125d267cebbbda2708003bd3e3e6b4da6
Seven-milk <786909151@qq.com>
2021-06-16 15:33:41 +0800

    test max func

--- C/test01/main.c
+++ C/test01/main.c
@@ -19,21 +19,20 @@
 #include <math.h>
 
 void main()
+
 {
-	int a[3][4]={1,2,3,4,5,6,7,8,9,10,11,12}, b[4][3];
-	for(int i=0; i<3; i++)
-	{
-		for(int j=0; j<4; j++)
-		{
-			b[j][i]=a[i][j];
-		}
-	}
-	for (int i=0; i<3; i++)
-	{
-		printf("%d %d %d %d\n", a[i][0], a[i][1], a[i][2], a[i][3]);
-	}
-	for(int i=0; i<4; i++)
-	{
-		printf("%d %d %d\n", b[i][0], b[i][1], b[i][2]);
-	}
+	double max(double x, double y);
+	double x, y, z;
+
+	printf("input x and y to compare\n");
+	scanf("%lf%lf", &x, &y);
+	z=max(x, y);
+	printf("the big one=%lf\n", z);
+}
+
+double max(double x, double y)
+{
+	double z;
+	z = x > y ? x : y;
+	return(z);
 }
